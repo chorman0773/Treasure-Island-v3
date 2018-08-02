d2c00ac389cb6589079c6fc6cb2a6ef4606d3e86
InfernoDeity <chorman64@gmail.com>
2018-08-02 15:33:45 -0400

    Update TGI

--- TextComponent.cpp
+++ TextComponent.cpp
@@ -4,19 +4,20 @@
 using std::string;
 using std::to_string;
 
-    TextComponent::TextComponent():text(),c(Color::NONE),endl(false),bg(false){
+    TextComponent::TextComponent():__tab(false),text(),c(Color::NONE),endl(false),bg(false){
         
     }
-    TextComponent::TextComponent(const string& text):text(text),c(Color::NONE),endl(false),bg(false){}
-    TextComponent::TextComponent(string&& text):text(text),c(Color::NONE),endl(false),bg(false){}
-    TextComponent::TextComponent(Color color):text(),c(color),endl(false),bg(false){}
+    TextComponent::TextComponent(const string& text):__tab(false),text(text),c(Color::NONE),endl(false),bg(false){}
+    TextComponent::TextComponent(string&& text):__tab(false),text(text),c(Color::NONE),endl(false),bg(false){}
+    TextComponent::TextComponent(Color color):__tab(false),text(),c(color),endl(false),bg(false){}
     TextComponent::TextComponent(int i):TextComponent(to_string(i)){}
     TextComponent::TextComponent(double d):TextComponent(to_string(d)){}
     TextComponent::TextComponent(bool b):TextComponent((b?"true":"false")){}
     TextComponent::TextComponent(const UUID& u):TextComponent(u.toString()){}
     TextComponent::TextComponent(Version v):TextComponent(v.toString()){}
-    TextComponent::TextComponent(endline_t):endl(true),c(Color::NONE),bg(false){}
-    TextComponent::TextComponent(const char* c):text(c),c(Color::NONE),endl(false),bg(false){}
+    TextComponent::TextComponent(endline_t):__tab(false),endl(true),c(Color::NONE),bg(false){}
+    TextComponent::TextComponent(tab_t):__tab(true),endl(false),c(Color::NONE),bg(false){}
+    TextComponent::TextComponent(const char* c):__tab(false),text(c),c(Color::NONE),endl(false),bg(false){}
 
     Color TextComponent::getColor()const{
         return c;
@@ -29,4 +30,7 @@ using std::to_string;
     }
     bool TextComponent::isBGColor()const{
         return bg;
+    }
+    bool TextComponent::isTab()const{
+        return __tab;
     }
